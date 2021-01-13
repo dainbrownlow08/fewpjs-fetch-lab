6ac4d830c81d2e90ec80d237ea0506205a7fc164
dainbrownlow08 <dain.brownlow@outlook.com>
2021-01-13 11:34:58 -0800

    this

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-function fetchBooks() {
+let books_arr;
 
+function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
 }
 
 function renderBooks(books) {
@@ -12,5 +16,6 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
+   fetchBooks();
 })
+
